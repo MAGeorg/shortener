7692200883809335936cf6b9730d10e58f00d0a4
Georgy Moskalev <Georgy.Moskalev@cyberprotect.ru>
2024-03-14 09:08:34 +0300

    iter5

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"flag"
+	"os"
 )
 
 type Config struct {
@@ -12,6 +13,12 @@ type Config struct {
 var Conf Config
 
 func init() {
+	if os.Getenv("SERVER_ADDRESS") != "" || os.Getenv("BASE_URL") != "" {
+		Conf.Address = os.Getenv("SERVER_ADDRESS")
+		Conf.BaseAddress = os.Getenv("BASE_URL")
+		return
+	}
+
 	flag.StringVar(&Conf.Address, "a", "localhost:8080", "Address for run server")
 	flag.StringVar(&Conf.BaseAddress, "b", "http://localhost:8080", "Base URL for shortener address")
 	flag.Parse()
